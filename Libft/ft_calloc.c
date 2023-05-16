319be550a3fbeb2b44b399a85754964fef634057
Guillem Forns Sant <gforns-s@car11s2.42barcelona.com>
2023-05-16 17:49:19 +0200

    pdt

---
+++ Libft/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 17:21:24 by gforns-s          #+#    #+#             */
+/*   Updated: 2023/05/16 17:48:51 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)  //calloc es un malloc lleno de '\0' like bzero
+{
+	
+}
+
+#include <stdlib.h>
+
+int	main(void)
+{
+	size_t	count 	= ;
+	size_t	size 	= ;
+}
