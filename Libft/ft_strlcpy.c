46b22c6dc9faef6ecf84b0e943260297bc70f32c
Guillem Forns Sant <gforns-s@cbr5s1.42barcelona.com>
2023-05-19 14:39:56 +0200

    fix strlcpy and add strljoin

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 10:57:50 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/05/12 12:40:57 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/05/19 14:38:08 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ size_t	ft_strlen(const char *var)
 	return (count);
 }*/
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, char const *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	z;
@@ -32,8 +32,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (dstsize == 0)
 		return ((unsigned long)z);
 	if (dst == 0 || src == 0)
-		return ((unsigned long)z);
-	if (dst < src)
 		return ((unsigned long)z);
 	while (i < dstsize -1 && i < z)
 	{
