5853f969993ede0473f15e8d60c265ac3aa976d9
BabyWillfred <132559287+BabyWillfred@users.noreply.github.com>
2023-05-22 15:47:03 +0000

    work in progress

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:42:10 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/05/19 20:24:46 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:43:15 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,76 +19,43 @@
 //Elimina todos los caracteres de la string ’set’ desde el principio y desde el final de ’s1’,
 //hasta encontrar un caracter no perteneciente a ’set’. La string resultante se devuelve con una reserva de malloc(3)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-char	*ft_copym(char *dst, char *s1, size_t possrc, size_t posdst)
+size_t	ft_check(const char *s1, const char *set, size_t i)
 {
-	if (dst[posdst] != '\0')
-		dst[posdst] = s1[possrc];
-	return (dst);
+	size_t	j;
+
+	j = 0;
+	while (s1[i] == set[j])
+		i++;
+	return (i);
 }
 
-size_t	ft_checker(char *s1, char const *set, size_t i)
-{
-	size_t	a;
-
-	a = 0;
-	while (s1[i] != set[a] && set[a] != '\0')
-		a++;
-	if (s1[i] == set[a])
-		return (0);
-	return (1);
-}
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	lens1;
-	char	*news1;
-	size_t	news1pos;
 	size_t	i;
-	char	*s1c;
+	size_t	j;
+	char	*s2;
+	size_t	len2;
 
-	s1c = (char *)s1;
+	len2 = ft_strlen(s1);
+	s2 = s2[len2 +1];
 	i = 0;
-	lens1 = ft_strlen(s1);
-	news1pos = 0;
-	news1 = malloc(lens1 + 1 * sizeof(char));
-	while (s1c[i] != '\0')
+	j = 0;
+	while(s1[i] != set[0] && s1[i] != '\0' && set[0] != '\0')
 	{
-		if (ft_checker(s1c, set, i) == 1)
-		{
-			ft_copym(news1, s1c, i, news1pos);
-			news1pos++;
-			i++;
-		}
-		if (ft_checker(s1c, set, i) == 0)
-			i++;
+		s2[j] = s1[i];
+		i++;
+		j++;
 	}
-	news1[news1pos] = '\0';
-	return (news1);
+	if (s1[i] == set[0])
+		i = ft_check(s1, set, i);
+	while(s1[i] != '\0')
+	{
+		s2[j] = s1[i];
+		i++;
+		j++;
+	}
+	//s2[j] = '\0';
+	return (s2);
 }
 
 #include <stdio.h>
@@ -100,4 +67,3 @@ int	main(void)
 
 	printf("%s\n", ft_strtrim(s1, set));
 }
-*/
