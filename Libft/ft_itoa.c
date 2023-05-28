093e2ccf1b5668860dc7435abc0aadba7baaf53e
Guillem Forns Sant <gforns-s@cbr4s6.42barcelona.com>
2023-05-28 19:15:21 +0200

    ok but too many lines

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 11:43:24 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/05/28 18:43:21 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/05/28 19:14:53 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 long int	ft_checksign(long int nb, long int x)
 {
 	if (nb < 0)
-		x = 2;
+		x = 1; // si le pongo 2 (signo y null) me peta en 1 test por ko.
 	if (nb >= 0)
 		x = 1;
 	return (x);
@@ -53,7 +53,6 @@ char	*ft_itoa(int n)
 	long int		x;
 	char			*s;
 	long int		z;
-	long int		i;
 	long int		num;
 
 	nb = n;
@@ -66,24 +65,22 @@ char	*ft_itoa(int n)
 	s = malloc ((z + x) * sizeof(char));
 	if (!s)
 		return (NULL);
-	i = ft_countdigits(nb);
-	s[i] = '\0';
-	i--;
+	s[z] = '\0';
+	z--;
 	while (z + 1 > 0)
 	{
-		s[i] = ft_intchar(num);
+		s[z] = ft_intchar(num);
 		num /= 10;
 		z--;
-		i--;
 	}
 	if (nb < 0)
 		s[0] = '-';
 	return (s);
 }
-
 /*
+
 int	main(void)
 {
-	printf("%s\n", ft_itoa(-));
+	printf("%s\n", ft_itoa(-12321323));
 }
 */
