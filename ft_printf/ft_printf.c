d2c1b4971df2759a40dae403cbefc49752148775
Guillem Forns Sant <gforns-s@car6s3.42barcelona.com>
2023-06-13 17:24:53 +0200

    .

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 16:12:35 by gforns-s          #+#    #+#             */
+/*   Updated: 2023/06/13 17:01:56 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *, ...)
+
+
+
+///////////////////
+
+%d, %i - numeros
+%x %X  - hexadecimal
+%c - caracter
+%s - stringg
+%p - puntero
+%u unsigned int
+%% %t %e %l
+
+printf("numero: %d, nombre: %s", edad, nombre)
+
+
+if (d o i)
+else if(x o X)
+else if (s)
+else if (c)
+else (write caracter de despues de %)
