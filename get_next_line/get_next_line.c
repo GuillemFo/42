fd87efaf2dda83246942bb495af5468e3b15205d
Guillem Forns Sant <gforns-s@cbr14s3.42barcelona.com>
2023-06-19 09:42:45 +0200

    pending

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:33:49 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/06/16 18:16:16 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/06/19 09:42:09 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,58 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *var)
+{
+	size_t	count;
+
+	count = 0;
+	while (var[count] != '\0')
+		count++;
+	return (count);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	s1l;
+	char	*str;
+	int		i;
+
+	i = 0;
+	s1l = ft_strlen(s1);
+	str = malloc (s1l + 1 * sizeof(char));
+	if (!str)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
 
 
 char	*get_next_line(int fd)
 {
-	char			buff[BUFFER_SIZE];
-	int				i;
-	int				j;
 	static char		tmp[BUFFER_SIZE];
-	char			newstr[BUFFER_SIZE];
+	static int		i = 0;
+	int				j;
+	char			buff[BUFFER_SIZE];
+	char			*newstr;
 
-	i = 0;
 	j = 0;
-	read(fd, buff, BUFFER_SIZE);
-	while (i < BUFFER_SIZE)
+	read(fd, tmp, BUFFER_SIZE);
+	while (tmp[i] != '\n' && tmp[i] != '\0' && i < BUFFER_SIZE)
 	{
-		while (buff[i] != '\n')
-		{
-			newstr[i] = buff[i];
-			i++;
-		}
+		buff[j] = tmp[i];
 		i++;
-		newstr[i] = buff[i];
-		i++;
-		while (buff[i] != '\0')
-		{
-			tmp[j] = buff[i];
-			i++;
-			j++;
-		}
-		tmp[j] = buff[i];
+		j++;
 	}
-	close (fd);
+	buff[j] = tmp[i];
+	buff[j +1] = '\0';
+	newstr = ft_strdup(buff);
 	return (newstr);
 }
 
@@ -59,7 +79,7 @@ int	main(void)
 	m = 0;
 	path = "/Users/gforns-s/Desktop/GITHUB/get_next_line/test.txt";
 	fd = open(path, O_RDONLY);
-	while (m < 10)
+	while (m < 20)
 	{
 		printf("%s", get_next_line(fd));
 		m++;
