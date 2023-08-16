8dcb5fa90935c02b23c4945bff12d3b513b02290
Guillem Forns Sant <gforns-s@car12s3.42barcelona.com>
2023-08-16 16:28:31 +0200

    work in progress

--- Push_Swap/push_swap.c
+++ Push_Swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 12:34:33 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/08/14 16:52:12 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/08/16 16:24:08 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int	ft_checkarg(char *argv)
 	}
 	else
 		return (write(2, "ERROR\n", 6));
+	return (1);
 }
 
 long	ft_atol() ascii to long.
@@ -50,17 +51,37 @@ long	ft_atol() ascii to long.
 
 int	main(int argc, char *argv[])
 {
-	argv = NULL;
-	if (argc < 2)
+	char **split_result;
+	if (argc < 2) 
 		return (write(2, "ERROR\n", 6));
+	else
+		*split_result = ft_split(argv[1]);
+		/*IMPLEMENT SPLIT HERE AND WORK WITH THE RESULTANT VALUES / MATRIX OF SPLIT & COUNT ALL VALUES RESULTED TO KNOW HOW MANY TIMES ITER THE WHILE LOOP FOR FT_CHECKARG*/
+		while ()
+		{
+			z = ft_checkarg(argv[x]);
+			if (z == 1)
+			{
+				write(1, "all ok\n", 7);
 
+			}
+			else
+			{
+				write(1, "error\n", 6);
+
+			}
+		}
 }
+
 
 /*
 
 #pasar char argv a long para comprobar que no sea mayor que int.(programar atol) luego filtrar a max int y min int dentro del atol y si supera, imprimir error.
 ----
 char comparar si es 10num o mas y filtrar max int y min int y luego atoi para trabajar con los datos.
+
+
+APLICAR SPLIT ANTES DE NADA PARA PODER TRABAJAR CON VALORES EN " " Y SOLOS A DEMAS DE CONSEGUIR UN VALOR PARA PODER ITERAR EL WHILE
 
 */
 
