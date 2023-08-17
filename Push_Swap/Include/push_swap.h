660ad24f38ca4374e10e127eafa02c1077ed65fc
Guillem Forns Sant <gforns-s@car11s2.42barcelona.com>
2023-08-17 14:04:12 +0200

    included limits lib

--- Push_Swap/Include/push_swap.h
+++ Push_Swap/Include/push_swap.h
@@ -6,20 +6,22 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 12:36:09 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/08/14 11:13:08 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/08/17 12:24:18 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "libft.h"
+# include <limits.h>
+
 typedef struct s_stack
 {
-	int			content;
+	int				content;
 	struct s_stck	*next;
 }	t_stack;
 
-# include "libft.h"
 
 
 
