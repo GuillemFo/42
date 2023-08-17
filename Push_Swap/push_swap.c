f8179aaea469ab2f1a9a628a4eeec79d012798c9
Guillem Forns Sant <gforns-s@car11s2.42barcelona.com>
2023-08-17 14:03:37 +0200

    added checkers for min max and split counter, missing itertion and control of split returned values

--- Push_Swap/push_swap.c
+++ Push_Swap/push_swap.c
@@ -6,11 +6,24 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 12:34:33 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/08/16 17:26:33 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/08/17 14:02:17 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+//int	**split_counter() might use ft_counter from split functions.  problem is split returns **s and counter counts only *s DO NOT USE, WE NEED INT.
+
+int	ft_split_counter(char **split)
+{
+	int	x;
+
+	x = 0;
+	while (split[x])
+		x++;
+	return (x);
+}
+
 
 int	ft_check_arg_is_num(char *argv)
 {
@@ -21,13 +34,11 @@ int	ft_check_arg_is_num(char *argv)
 	i = 1;
 	if (argv[0] == '-' || argv[0] >= '0' && argv[0] <= '9')
 	{
-	/*	if (argv[1] != '\0' || argv[1] < '0' && argv[1] > '9')
-			return (write(2, "ERROR\n", 6)); */
 		while (argv[i] != '\0')
 		{
 			z = ft_isdigit(argv[i]);
 			if (z == 0)
-				return (write(2, "ERROR\n", 6));
+				return (write(2, "ERROR FT_ISDIGIT REURNED FALSE\n", 32));
 			i++;
 		}
 	}
@@ -36,53 +47,80 @@ int	ft_check_arg_is_num(char *argv)
 	return (1);
 }
 
-/* NEED TESTER TO CHECK WITH LONG IF ITS OVER OR UNDER MAX AND MIN INTS AND BE ABLE TO RETURN A CODE TO SEE IF ITS WRONG*/
+/* NEED TESTER TO CHECK WITH LONG IF ITS OVER OR UNDER MAX AND MIN INTS AND BE ABLE TO RETURN A CODE TO SEE IF ITS WRONG  (DONE)*/
 /* THEN APPLY ATOI TO WORK WITH THE VALUES WITH THE PUSH_SWAP*/
 
+long	ft_check_max_min(char *argv)
+{
+	long	result;
+	long	sign;
+	int		i;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	if (argv[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (argv[i])
+	{
+		result = result * 10 + argv[i] - '0';
+		i++;
+	}
+	result = sign * result;
+	if (result > INT_MAX || result < INT_MIN)
+		return (0);
+	return (1);
+}
 
 
 int	main(int argc, char *argv[])
 {
-	char **split_result;
+	char	**split_result;
+	int		x;
+
 	if (argc < 2) 
 		return (write(2, "ERROR\n", 6));
 	else
 		if (argc == 2)
 		{
-			*split_result = ft_split(argv[1]);
-/*IMPLEMENT SPLIT HERE AND WORK WITH THE RESULTANT VALUES / MATRIX OF SPLIT & COUNT ALL VALUES RESULTED TO KNOW HOW MANY TIMES ITER THE WHILE LOOP FOR FT_CHECKARG*/
-		while ( /*calcular el numero de splits para saber cuanto iterar*/)
-		{
-			z = ft_check_arg_is_num(argv[x]);
-			if (z == 1)
-			{
-				write(1, "all ok\n", 7);
-				/*añadir iterador para avanzar en el array*/
-			}
-			else
-			{
-				write(1, "error\n", 6);
-				/*frorzar salida p*/
-			}
-		}
-
-		else
-		{
-			while (argc > 2)
+			split_result = ft_split(argv[1], ' ');
+			x = ft_split_counter(split_result);
+		/*IMPLEMENT SPLIT HERE AND WORK WITH THE RESULTANT VALUES / MATRIX OF SPLIT & COUNT ALL VALUES RESULTED TO KNOW HOW MANY TIMES ITER THE WHILE LOOP FOR FT_CHECKARG*/
+			while (/*calcular el numero de splits para saber cuanto iterar*/)
 			{
 				z = ft_check_arg_is_num(argv[x]);
 				if (z == 1)
 				{
-					write(1, "all ok\n", 7);
-					argc--;
+					write(1, "all ok on arg is num = 2\n", 27);
+					/*añadir iterador para avanzar en el array*/
 				}
 				else
 				{
-					write(1, "error\n", 6);
-					argc = 0;
+					write(1, "error on arg is num = 2\n", 25);
+					/*frorzar salida p*/
 				}
 			}
 		}
+	else
+	{
+		while (argc > 2)
+		{
+			z = ft_check_arg_is_num(argv[x]);
+			if (z == 1)
+			{
+				write(1, "all ok on arg is num > 2\n", 27);
+				argc--;
+			}
+			else
+			{
+				write(1, "error on arg is num > 2\n", 25);
+				argc = 0;
+			}
+		}
+	}
 }
 
 
